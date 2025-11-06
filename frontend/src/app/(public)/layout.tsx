404e3c42713fec9f714e78b08042fe06e1a6de04
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-11-06 12:58:25 +0700

    chore(FE): move layout and page into (public) folder

---
+++ frontend/src/app/(public)/layout.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "../_components/footer";
+import { TopNav } from "../_components/topnav";
+
+export default function PublicLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="min-h-screen">
+      <TopNav />
+      {children}
+      <Footer />
+    </div>
+  );
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { TopNav } from "./_components/topnav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,10 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen">
-          <TopNav />
-          {children}
-        </div>
+        {children}
       </body>
     </html>
   );
